dbd40731ca4dc7422f8de814d4f64b0ddbe76e18
Andy Jung <jwindow99@gmail.com>
2024-02-18 16:10:44 -0600

    change

--- SpringHibernateTest/src/main/java/com/jung/associatemapping/Item.java
+++ SpringHibernateTest/src/main/java/com/jung/associatemapping/Item.java
@@ -3,8 +3,6 @@ package com.jung.associatemapping;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
